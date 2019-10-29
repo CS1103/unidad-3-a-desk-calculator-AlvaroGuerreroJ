44dd74ea29acc4d369c15529898e8697b8b22944
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2019-10-29 10:40:40 -0500

    Link with readline

--- src/SConscript.py
+++ src/SConscript.py
@@ -4,6 +4,7 @@ Import('base_env')
 
 src_env = base_env.Clone()
 
+src_env.Append(LIBS='readline')
 src_env['OBJPREFIX'] = src_env['OBJPREFIX'] + 'src/'
 
 src_env.Program(target='DesktopCalculator', source=Glob('*.cpp'))
